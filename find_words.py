26fe3fc3b803a72c408098caa6fdbd20cd00fccb
sux2000 <saverioolorusso@gmail.com>
2024-12-20 13:48:21 +0100

    Update find_words.py

--- find_words.py
+++ find_words.py
@@ -1,5 +1,5 @@
 
-file = open("C:/Users/saver/OneDrive/Desktop/file_python.txt", 'r')
+file = open("filepath", 'r')
 #word = word that we want find
 #text = file.read() #it give me like a unique string so we aren't use it
 
