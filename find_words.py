3c137d50df119bc1cd3c856e0e3ef1252b841622
sux2000 <saverioolorusso@gmail.com>
2024-12-20 13:47:11 +0100

    Update find_words.py

--- find_words.py
+++ find_words.py
@@ -1,10 +1,10 @@
 
 file = open("C:/Users/saver/OneDrive/Desktop/file_python.txt", 'r')
-
+#word = word that we want find
 #text = file.read() #it give me like a unique string so we aren't use it
 
 for line in file:
-    if "pizza" in line:
+    if "word" in line:
         print(line.strip())
 
 file.close()
